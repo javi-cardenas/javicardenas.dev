2701df944d3f7997822796ad3ba9a137d7f29dca
Javi Cardenas <85462046+javi-cardenas@users.noreply.github.com>
2025-07-09 20:57:43 -0400

    react tutorial

---
+++ src/pages/about.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from '@theme/Layout';
+import Button from '../components/Button';
+
+export default function MyReactPage() {
+  return (
+    <Layout>
+      <h1>My React page</h1>
+      <p>This is a React page</p>
+      <Button />
+    </Layout>
+  );
+}
